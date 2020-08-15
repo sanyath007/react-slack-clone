051adb127efbae507ac28cea8d331d234770bc69
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-15 11:16:37 +0700

    Modified Sidebar component with using Hooks to get data from firebase

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import SidebarOption from '../SidebarOption/SidebarOption';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
@@ -13,8 +13,20 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import db from '../../firebase';
 
 function Sidebar() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot(snapshot => {
+      setChannels(snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().name
+      })));
+    });
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -37,7 +49,9 @@ function Sidebar() {
       <SidebarOption Icon={ExpandLessIcon} title={'Show less'} />
       <hr />
       <SidebarOption Icon={ExpandMoreIcon} title={'Channels'} />
-      <SidebarOption title={'Youtube'} />
+      {channels.map(ch => (
+        <SidebarOption key={ch.id} title={ch.name} />
+      ))}
       <hr />
       <SidebarOption Icon={AddIcon} title={'Add Channel'} />
     </div>
