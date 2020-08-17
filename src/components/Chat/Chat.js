1b5692d9a43ca18076a6762c411befd68391f27a
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-18 05:33:54 +0700

    Used useEffect Hook to get room details in Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../../firebase';
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+  
+  useEffect(() => {
+    if(roomId) {
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot(snapshot => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
 
   return (
     <div className="chat">
